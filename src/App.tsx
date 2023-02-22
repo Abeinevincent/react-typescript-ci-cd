66edc460fe84e490afd1cba0ec19e7586c77ed96
Abeinevincent <abeinevincent@gmail.com>
2023-02-22 18:15:56 +0300

    Initial Content

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Initial Content</h2>
+        <p>
+          This content is only for testing. Please replace it with your website
+          content
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
